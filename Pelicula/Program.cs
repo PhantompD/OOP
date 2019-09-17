1a9c644ea5bd9f8072479a721155aa6cc9ce9ea6
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-09-17 01:52:41 -0700

    Update Program.cs

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -6,7 +6,19 @@ namespace Pelicula{
 //Clase Pelicula
 class Peli{
 private string Titulo;
-private Int16 Año;  
+private int Año;  
+//private string pais;
+//private string director;
+
+//Constructor #1
+Peli(){}
+
+//Constructor #2
+public Peli(string T, int A)
+{
+  this.Titulo=T;
+  this.Año=A;
+}
 
 public void SetTitulo(string titulo)
 {
@@ -28,18 +40,26 @@ public int GetAño()
        return this.Año;
 }
 
+public void Imprime()
+{
+ //Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+
+}
+
 static void Main(){
 
 
     Peli p1 = new Peli();
     p1.SetTitulo("La La Land");
     p1.SetAño(2016); 
-    Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+    p1.Imprime();
+   
 
 
 
 
 }
+
 
 
 
