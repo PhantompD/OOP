3ffa69a9e8d44bf83cfdb0d0859bbdc7340e92d5
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-10-08 22:34:05 -0700

    Domino

---
+++ Domino/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Domino
+{
+    class Program
+    {
+
+      class Domino
+      {
+        public readonly int LadoA;
+        public readonly int LadoB;
+
+       public Domino(int LA, int LB)
+       {
+           LadoA=LA;
+           LadoB=LB;
+       }
+          
+          public static int operator +(Domino a, Domino b)
+          {
+              return (a.LadoA + a.LadoB + b.LadoB + b.LadoA);
+          }
+
+      }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Domino a = new Domino (2,0);
+            var b = new Domino (4,2);
+            Console.WriteLine(a + b);
+        }
+    }
+}
