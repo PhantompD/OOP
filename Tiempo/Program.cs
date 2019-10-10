885f5bb35937899ff9438f4245a0aa8b128d8303
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-10-09 23:42:18 -0700

    Duracion

---
+++ Tiempo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tiempo
+{
+    class Program
+    {
+        class Duracion
+        {
+
+          public double Horas;
+          public double Minutos;
+          public double Segundos;
+
+          public Duracion (double H, double M, double S)
+          {
+            Horas=H;
+            Minutos=M;
+            Segundos=S;
+          }
+
+           public void print()
+           {
+
+            Console.WriteLine("Horas:{0}", Horas + (Minutos/60) + (Segundos/3600));
+
+           }
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Duracion a = new Duracion (2,10,5);
+            a.print();
+        }
+    }
+}
