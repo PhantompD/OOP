e737a6601c7506e9588972f6e4aeb6f77821fe3d
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-10-22 21:42:55 -0700

    CreardeNuevo

--- Parametros/Program.cs
+++ Parametros/Program.cs
@@ -1,52 +1,12 @@
 ﻿using System;
 
-namespace Anon
+namespace Parametros
 {
-    class Duplicador
-    {
-	public void duplica(int n) 
-    { 
-			n = n*2;
-	}
-    
-    }
-    class Persona
-    {
-	    public string nombre;
-	    public string apellido;
-
-
-	    public Persona() {
-		nombre = "Rogers";
-		apellido = "Rabbit";
-	    }
-    }
-
-    class Anonymous
-    {
-	    static public void anonimo(Persona p) { 
-			p.nombre = "xxxxxxxxx";
-			p.apellido = "xxxxxxxxx";
-	    }
-        static void Main(){
-		Persona Anon = new Persona();
-
-		Console.WriteLine(Anon.nombre);
-		anonimo(Anon);
-		Console.WriteLine(Anon.nombre);
-	    }
-    }
-
     class Program
     {
-        static void Main()
-	    {
-		Duplicador d = new Duplicador();
-		int x = 2;
-		d.duplica(x);
-		Console.WriteLine(x);
-		d.duplica_ref(x);
-		Console.WriteLine(x);
-	    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
