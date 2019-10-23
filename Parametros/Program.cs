3030ec632a1a5a754cf553eadac01e8c6aa32973
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-10-22 22:06:11 -0700

    ref

--- Parametros/Program.cs
+++ Parametros/Program.cs
@@ -4,9 +4,45 @@ namespace Parametros
 {
     class Program
     {
+      class Cuadrado
+      {
+          public void cuadrado(int n) 
+          { 
+			n = n*n;
+	      }
+
+	      public void cuadradoR(ref int n) 
+          { 
+			n = n*n;
+	      }
+
+
+       }
+
+
+
+        
+        
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Parametros por ref
+            Cuadrado q = new Cuadrado();
+		    int x = 3;
+            Console.WriteLine("con x = 3");
+		    q.cuadrado(x);
+		    Console.WriteLine("Pase de parametros:" + x);
+
+		    q.cuadradoR(ref x);
+		    Console.WriteLine("Pase de parametros por ref:" + x);
         }
+
+
+
+
+
+
+        
     }
 }
