202e03971f6853ba02f3df358c8bbdb3f2aac4de
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-10-23 19:10:18 -0700

    Bajista

--- Músicos/Program.cs
+++ Músicos/Program.cs
@@ -2,11 +2,60 @@
 
 namespace Músicos
 {
+    
+    class Músico
+    {
+      protected string nombre;
+
+      public Músico(string n)
+      {
+       nombre = n;
+      }
+
+      public virtual void Afina()
+      {
+       Console.WriteLine("{0} afina su instrumento", nombre );
+      } 
+
+      public virtual void Saludo()
+      {
+          Console.WriteLine("Hola soy {0}", nombre);
+      }
+
+    }
+
+    class Bajista : Músico 
+    {
+        private string bajo;
+
+        public Bajista(string no, string b):base(no)
+        {
+            bajo = b;
+        }
+
+        public override void Afina()
+        {
+            Console.WriteLine("{0} afinando su {1}", nombre, bajo);
+        }
+
+        public override void Saludo()
+        {
+            Console.WriteLine("Hola soy {0} y soy bajista", nombre);
+        }
+    }
+    
+    
+    
+    
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Bajista b = new Bajista("Dan", "Banjoo");
+            b.Saludo();
+            b.Afina();
         }
     }
 }
