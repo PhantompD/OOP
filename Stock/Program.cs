7da7795f34cd3e99208ec7101a58ff61ba91a9b8
PhantompD <54628759+PhantompD@users.noreply.github.com>
2019-11-18 18:51:13 -0800

    constructor

---
+++ Stock/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Stock
+{
+    
+    class Stock<T>
+    {
+       
+     public T[] elementos;
+     readonly int max_size;
+     public int apuntador=0;
+    
+    
+     public Stock():this(100)
+     {
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
